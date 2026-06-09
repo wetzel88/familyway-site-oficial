cc2726b00b2a862c286ba5210f57ade20daa47c1
Felipe Wetzel <wetzel.88@gmail.com>
2026-06-09 22:04:28 +0200

    Cria endpoint de contato

---
+++ api/contact.ts
@@ -0,0 +1,8 @@
+import { Resend } from "resend";
+
+export default async function handler(req: any, res: any) {
+  return res.status(200).json({
+    success: true,
+    message: "API funcionando",
+  });
+}
